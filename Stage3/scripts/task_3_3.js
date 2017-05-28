491155906d9265f478906da4be0d278372363346
LR <1020251822@qq.com>
2017-05-28 23:48:14 +0800

    added task_3_3

---
+++ Stage3/scripts/task_3_3.js
@@ -0,0 +1,51 @@
+var stu=document.getElementById('student');
+var staff=document.getElementById('nonstudent');
+//在校生和非在校生之间的联动
+stu.onclick=function(){
+  var company=document.getElementById('company');
+  if (company.className=='') {
+    company.className='initHidden';
+  }
+  var school=document.getElementById('school');
+  var location=document.getElementById('location');
+  var beijing=document.getElementById('beijing');
+  school.className='';
+  location.className='';
+  beijing.className='';
+}
+staff.onclick=function(){
+  var school=document.getElementById('school');
+  if (school.className=='') {
+    school.className='initHidden';
+  }
+  var company=document.getElementById('company');
+  company.className='';
+}
+//城市和对应大学之间的联动
+var locas=document.getElementById('location');
+
+locas.onchange=function(){
+  var school=document.getElementById('school');
+  var list=school.getElementsByTagName('select');
+  var value=locas.value;
+  for(var i=0;i<locas.children.length;i++){
+    if (locas.children[i].value==value) {
+      var index=i;
+    }else {
+      list[i+1].className='initHidden';
+    }
+  }  
+  list[index+1].className='';
+}
+// for (var i = 0; i < locas.children.length; i++) {
+//   // if (locas.children[i].getAttribute("selected")=="selected") {
+//   //   var school=document.getElementById('school');
+//   //   var list=school.getElementsByTagName('select');
+//   //   list[i+1].className='';
+//   // }
+//   locas.children[i].onchange=function(){
+//     var school=document.getElementById('school');
+//     var list=school.getElementsByTagName('select');
+//     list[i+1].className='';
+//   };
+// }
